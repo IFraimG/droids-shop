971f97f18cc40f8518bfa694a1d6b3e586338488
IFraimG <kulsasha55@gmail.com>
2026-01-27 14:01:52 +0300

    create microfrontend part remote

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
 module.exports = {
+  publicPath: "auto",
   devServer: {
     proxy: {
       "^/api": {
@@ -6,5 +9,33 @@ module.exports = {
         pathRewrite: { "^/api": "" }
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "droids_shop_remote",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./RemoteApp": "./src/remote-entry.js"
+        },
+        shared: {
+          vue: {
+            singleton: true,
+            eager: true,
+            requiredVersion: "^2.6.11"
+          },
+          "vue-router": {
+            singleton: true,
+            eager: true,
+            requiredVersion: "^3.1.6"
+          },
+          vuex: {
+            singleton: true,
+            eager: true,
+            requiredVersion: "^3.1.3"
+          }
+        }
+      })
+    ]
   }
 };
